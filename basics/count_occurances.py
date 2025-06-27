33f8f4d35f1ff32dc72720d1d4032ffbd04b4fe1
Sandy <7pandeys@gmail.com>
2025-06-27 21:34:39 +0530

    Co-authored-by: Collaborator Name bhavishyapriyadarshini9@gmail.com

--- basics/count_occurances.py
+++ basics/count_occurances.py
@@ -14,3 +14,5 @@ def count_occurrence(var):
 print(count_occurrence(var))
 # output: {'a': 1, ' ': 7, 'l': 1, 'z': 1, 'y': 1, 'd': 1, 'o': 1, 'g': 1, 'j': 1, 'u': 1, 'm': 1, 'p': 1, 's': 1,
 # 'v': 1, 'e': 1, 'r': 1, 't': 1, 'h': 1, 'i': 1, 'n': 1, 'w': 1, 'f': 1}
+
+
