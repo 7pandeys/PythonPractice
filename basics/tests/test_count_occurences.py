243c2cdc639b1d80e1c24c16b87bc06769c28688
Sandy <7pandeys@gmail.com>
2025-07-01 06:46:38 +0530

    Add pytest and coverage

--- basics/tests/test_count_occurences.py
+++ basics/tests/test_count_occurences.py
@@ -3,9 +3,7 @@ from basics.count_occurences import count_occurrence
 def test_count_occurrence():
     input_str = "a lazy dog jumps over the sleeping wolf"
     expected = {
-        'a': 1, ' ': 7, 'l': 3, 'z': 1, 'y': 1, 'd': 1, 'o': 2, 'g': 1, 'j': 1, 'u': 1,
-        'm': 1, 'p': 1, 's': 2, 'v': 1, 'e': 3, 'r': 1, 't': 1, 'h': 1, 'n': 1, 'w': 1, 'f': 1, 'i': 1
+        'a': 2, ' ': 7, 'l': 3, 'z': 1, 'y': 1, 'd': 1, 'o': 3, 'g': 2, 'j': 1, 'u': 1, 'm': 1, 'p': 2, 's': 2, 'v': 1, 'e': 4, 'r': 1, 't': 1, 'h': 1, 'i': 1, 'n': 1, 'w': 1, 'f': 1
     }
-    {'a': 1, ' ': 7, 'l': 1, 'z': 1, 'y': 1, 'd': 1, 'o': 1, 'g': 1, 'j': 1, 'u': 1, 'm': 1, 'p': 1, 's': 1,
-    'v': 1, 'e': 1, 'r': 1, 't': 1, 'h': 1, 'i': 1, 'n': 1, 'w': 1, 'f': 1}
+
     assert count_occurrence(input_str) == expected
